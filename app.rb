cb9709bbddc3fbcc89802c2965ac69eda5be32f9
Christopher Wood <cjwood032@gmail.com>
2018-07-22 21:23:00 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,4 +6,8 @@ class App < Sinatra::Base
     @user_name = params[:name].to_s.reverse
     "#{@user_name}"
   end
+  get '/square/:number' do
+    @number = params[:number].to_i
+    "#{@number * @number}"
+  end
 end
